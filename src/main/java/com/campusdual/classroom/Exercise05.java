4aaf432f7b0bcd65273ef7701464694fa80d9fb2
Fran <franrego333@gmail.com>
2024-09-25 13:55:31 +0200

    Ejercicio 5 completado

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -1,28 +1,47 @@
 package com.campusdual.classroom;
 
 public class Exercise05 {
+
+
     public static void main(String[] args) {
-
+        int n1=5;
+        int n2=0;
+        int n3= -2;
+        checkLessOrGreaterEqual(n1,n2);
+        checkMultiply(n1,n2);
+        checkPositiveNegative(n3);
     }
 
-    //TODO ↓
-    // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
-    // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
-    public static void checkLessOrGreaterEqual(int n1, int n2) {
 
-    }
 
-    //TODO ↓
-    // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
-    // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
-    public static void checkMultiply(int n1, int n2) {
+        public static void checkLessOrGreaterEqual(int n1, int n2) {
+            if (n1 < n2) {
+                System.out.println(n1 + " es menor que " + n2);
+            } else if (n1 == n2) {
+                //esto está mal en el texto debería ser igual que
+                System.out.println(n1 + " es mayor o igual que " + n2);
+            } else{
+                System.out.println(n1 + " es mayor o igual que " + n2);
+            }
+        }
 
-    }
 
-    //TODO ↓
-    // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
-    // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
-    public static void checkPositiveNegative(int num) {
-        
-    }
+        public static void checkMultiply(int n1, int n2) {
+            if (n2 != 0 && n1 % n2 == 0) {
+                System.out.println(n1 + " es múltiplo de " + n2);
+            } else {
+                System.out.println(n1 + " no es múltiplo de " + n2);
+            }
+        }
+
+        public static void checkPositiveNegative(int n) {
+            if (n > 0) {
+                System.out.println(n + " es positivo.");
+            } else if (n == 0) {
+                System.out.println(n + " es positivo.");
+            } else {
+                System.out.println(n + " es negativo.");
+            }
+        }
 }
+
